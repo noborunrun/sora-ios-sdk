d0430e8a3efd70d12c99fb7a6e4ade58bbb005b3
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2016-03-31 14:56:41 +0900

    create a project

---
+++ Sora/Sora/Sora.h
@@ -0,0 +1,19 @@
+//
+//  Sora.h
+//  Sora
+//
+//  Created by 鈴木鉄也 on 2016/03/31.
+//  Copyright © 2016年 Shiguredo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sora.
+FOUNDATION_EXPORT double SoraVersionNumber;
+
+//! Project version string for Sora.
+FOUNDATION_EXPORT const unsigned char SoraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sora/PublicHeader.h>
+
+
