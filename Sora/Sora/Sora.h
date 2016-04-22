decd5482863cd15ae142bcda50172e13452ade7c
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2016-04-22 17:04:51 +0900

    create a new project file

--- Sora/Sora/Sora.h
+++ Sora/Sora/Sora.h
@@ -1,3 +1,11 @@
+//
+//  Sora.h
+//  Sora
+//
+//  Created by 鈴木鉄也 on 2016/04/20.
+//  Copyright © 2016年 Shiguredo Inc. All rights reserved.
+//
+
 #import <UIKit/UIKit.h>
 
 //! Project version number for Sora.
@@ -8,38 +16,4 @@ FOUNDATION_EXPORT const unsigned char SoraVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Sora/PublicHeader.h>
 
-#if !TARGET_OS_IPHONE
-#import <Sora/RTCNSGLVideoView.h>
-#endif
 
-#import <Sora/RTCAVFoundationVideoSource.h>
-#import <Sora/RTCAudioSource.h>
-#import <Sora/RTCAudioTrack.h>
-#import <Sora/RTCDataChannel.h>
-#import <Sora/RTCEAGLVideoView.h>
-#import <Sora/RTCFileLogger.h>
-#import <Sora/RTCI420Frame.h>
-#import <Sora/RTCICECandidate.h>
-#import <Sora/RTCICEServer.h>
-#import <Sora/RTCLogging.h>
-#import <Sora/RTCMediaConstraints.h>
-#import <Sora/RTCMediaSource.h>
-#import <Sora/RTCMediaStream.h>
-#import <Sora/RTCMediaStreamTrack.h>
-#import <Sora/RTCOpenGLVideoRenderer.h>
-#import <Sora/RTCPair.h>
-#import <Sora/RTCPeerConnection.h>
-#import <Sora/RTCPeerConnectionDelegate.h>
-#import <Sora/RTCPeerConnectionFactory.h>
-#import <Sora/RTCPeerConnectionInterface.h>
-#import <Sora/RTCSessionDescription.h>
-#import <Sora/RTCSessionDescriptionDelegate.h>
-#import <Sora/RTCStatsDelegate.h>
-#import <Sora/RTCStatsReport.h>
-#import <Sora/RTCTypes.h>
-#import <Sora/RTCVideoCapturer.h>
-#import <Sora/RTCVideoRenderer.h>
-#import <Sora/RTCVideoSource.h>
-#import <Sora/RTCVideoTrack.h>
-
-#import <Sora/SRWebSocket.h>
---
+++ Sora/SoraTests/SoraTests.m
@@ -0,0 +1,39 @@
+//
+//  SoraTests.m
+//  SoraTests
+//
+//  Created by 鈴木鉄也 on 2016/04/20.
+//  Copyright © 2016年 Shiguredo Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SoraTests : XCTestCase
+
+@end
+
+@implementation SoraTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
