f8006dd2aa4312852d016836ce4d8fcb632123a9
SUZUKI Tetsuya <szktty@shiguredo.jp>
2016-07-04 15:20:20 +0900

    design main view

--- examples/SoraDemo/SoraDemo/ViewController.swift
+++ examples/SoraDemo/SoraDemo/ViewController.swift
@@ -10,7 +10,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var remoteView: RTCEAGLVideoView!
     @IBOutlet weak var URLField: UITextField!
     @IBOutlet weak var ChannelIdField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var connectButton: UIButton!
+    var connection: SoraConnection!
 
     var state: State
     
@@ -27,6 +29,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.messageLabel.text = nil
         self.connectButton.setTitle("Connect", forState: UIControlState.Normal)
     }
 
@@ -36,17 +39,52 @@ class ViewController: UIViewController {
     }
 
     @IBAction func connectOrDisconnect(sender: AnyObject) {
+        NSLog("connectOrDisconnect")
         switch self.state {
         case .Connecting:
+            NSLog("disconnect")
             self.state = .Closed
             self.connectButton.setTitle("Connect", forState: UIControlState.Normal)
         case .Closed:
+            NSLog("try connect")
             self.state = .Connecting
             self.connectButton.setTitle("Disconnect", forState: UIControlState.Normal)
+            self.tryConnect()
         }
     }
     
+    func tryConnect() {
+        NSLog("try connect")
+        print("URL = %@", self.URLField.text)
+        print("ChannelId = %@", self.ChannelIdField.text)
+        if self.URLField.text == "" {
+            self.messageLabel.text = "Error: Input URL"
+            return
+        }
+        
+        if self.ChannelIdField.text == "" {
+            self.messageLabel.text = "Error: Input channel ID"
+            return
+        }
+        
+        NSLog("create SoraConnection")
+        let URL = NSURL(string: self.URLField.text!)!
+        let channelId = self.ChannelIdField.text!
+        self.connection = SoraConnection(URL: URL)
+        let req = SoraConnectRequest(role: SoraRole.Downstream, channelId: channelId, accessToken: nil)
+        self.connection.open(req!)
+    }
+    
     @IBAction func switchVideoView(sender: AnyObject) {
+        NSLog("switch video view")
+    }
+    
+    @IBAction func URLFieldEditingDidEndOnExit(sender: AnyObject) {
+        NSLog("URLFieldEditingDidEndOnExit")
+    }
+    
+    @IBAction func channelIdEditingDidEndOnExit(sender: AnyObject) {
+        NSLog("channelIdEditingDidEndOnExit")
     }
     
 }
