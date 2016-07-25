480ac3dd810d05c899b69dacf8f99c99bb8449d5
SUZUKI Tetsuya <szktty@shiguredo.jp>
2016-07-25 15:57:49 +0900

    prepare documents

--- Sora/Sora/SoraConnection.h
+++ Sora/Sora/SoraConnection.h
@@ -16,33 +16,95 @@
 
 @protocol SoraConnectionDelegate;
 
+/** Sora サーバーとの接続状態を表します。 */
 typedef NS_ENUM(NSUInteger, SoraConnectionState) {
+
+    /** シグナリング接続中です。 */
     SoraConnectionStateConnecting,
+    
+    /** シグナリング接続が完了し、シグナリングメッセージの通信が可能です。 */
     SoraConnectionStateOpen,
+    
+    /** ピア接続中です。 */
     SoraConnectionStatePeerConnecting,
+    
+    /** ピア接続が完了し、データ通信が可能です。 */
     SoraConnectionStatePeerOpen,
+    
+    /** シグナリング切断中です。 */
     SoraConnectionStateClosing,
+    
+    /** シグナリング接続していません。 */
     SoraConnectionStateClosed
 };
 
+/**
+ `SoraConnection` オブジェクトは Sora サーバーとシグナリング接続を行います。
+ Sora サーバーでは、シグナリングは WebSocket で JSON 形式のメッセージを介して行います。
+ */
 @interface SoraConnection : NSObject
 
+/** Sora サーバーの URL */
 @property(nonatomic, readonly, nonnull) NSURL *URL;
+
+/** Sora サーバーとの接続状態 */
 @property(nonatomic, readonly) SoraConnectionState state;
+
+/** デリゲート */
 @property(nonatomic, weak, readwrite, nullable) id<SoraConnectionDelegate> delegate;
+
 @property(nonatomic, readonly, nonnull) RTCPeerConnectionFactory *peerConnectionFactory;
+
+/**
+ ピア接続オブジェクト
+ 
+ @warning このオブジェクトのデリゲートを変更しないでください。
+ デリゲートは `SoraConnection` オブジェクトの状態管理に使われます。
+ */
 @property(nonatomic, readonly, nonnull) RTCPeerConnection *peerConnection;
 @property(nonatomic, readonly, nonnull) RTCFileLogger *fileLogger;
+
+/** メディアストリーム (RTCMediaStream オブジェクト) の配列 */
 @property(nonatomic, readonly, nonnull) NSArray *remoteStreams;
+
+/** 受信した映像を描画するオブジェクト (RTCVideoRenderer プロトコル) の配列 */
 @property(nonatomic, readonly, nonnull) NSArray *remoteVideoRenderers;
 
+/**
+ 指定のサーバーに接続し、初期化済みの `SoraConnection` オブジェクトを返します。
+
+ @param URL 接続するサーバーの URL
+ @param config ピア接続の設定
+ @param constraints メディアの制約
+ @return 初期化済みの `SoraConnection` オブジェクト
+ */
 - (nullable instancetype)initWithURL:(nonnull NSURL *)URL
                        configuration:(nullable RTCConfiguration *)config
                          constraints:(nullable RTCMediaConstraints *)constraints;
+
+/**
+ 指定のサーバーに接続し、初期化済みの `SoraConnection` オブジェクトを返します。
+
+ @param URL 接続するサーバーの URL
+ @return 初期化済みの `SoraConnection` オブジェクト
+ */
 - (nullable instancetype)initWithURL:(nonnull NSURL *)URL;
 
+/**
+ サーバーとシグナリング接続を行います。
+ 
+ @param connectRequest connect シグナリングメッセージ
+ */
 - (void)open:(nonnull SoraConnectRequest *)connectRequest;
+
+/** シグナリング接続を閉じます。 */
 - (void)close;
+
+/**
+ シグナリングメッセージを送信します。
+ 
+ @param message 送信するシグナリングメッセージ
+ */
 - (void)sendMessage:(nonnull SoraMessage *)message;
 
 + (nonnull RTCConfiguration *)defaultPeerConnectionConfiguration;
@@ -54,6 +116,9 @@ typedef NS_ENUM(NSUInteger, SoraConnectionState) {
 
 @end
 
+/**
+ `SoraConnectionDelegate` プロトコルは `SoraConnection` のデリゲートメソッドを定義しています。
+ */
 @protocol SoraConnectionDelegate <NSObject>
 
 @required
