b069c4d4969e1ca78fec5b87cd3f083ec85acce5
SUZUKI Tetsuya <szktty@shiguredo.jp>
2016-08-16 22:15:30 +0900

    impl JSON encoding

--- Sora/Sora/Message.swift
+++ Sora/Sora/Message.swift
@@ -1,21 +1,125 @@
 import Foundation
 import WebRTC
+import Argo
+import Curry
+import Runes
+
+func CreateJSONString(obj: AnyObject) -> String {
+    return try! String(data: NSJSONSerialization.dataWithJSONObject(obj, options: NSJSONWritingOptions(rawValue: 0)),
+                       encoding: NSUTF8StringEncoding)!
+}
+
+public protocol JSONEncodable {
+    
+    func JSONString() -> String
+
+}
+
+public enum Config<T: JSONEncodable> {
+    
+    case Default
+    case Enable(T)
+    case Disable
+    
+    func JSONString() -> String! {
+        switch self {
+        case .Default:
+            return nil
+        case .Enable(let value):
+            return value.JSONString()
+        case .Disable:
+            return "false"
+        }
+    }
+    
+}
 
 public enum Signaling {
     
     public enum Role {
+        
         case Upstream
         case Downstream
+        
+        public func JSONString() -> String {
+            switch self {
+            case .Upstream:
+                return "upstream"
+            case .Downstream:
+                return "downstream"
+            }
+        }
+        
     }
     
-    public enum VideoCodecType {
+    public enum VideoCodecType: JSONEncodable {
+        
         case VP8
         case VP9
         case H264
+        
+        public func JSONString() -> String {
+            switch self {
+            case .VP8:
+                return "VP8"
+            case .VP9:
+                return "VP9"
+            case .H264:
+                return "H264"
+            }
+        }
+        
     }
     
-    public enum AudioCodecType {
+    public struct Video: JSONEncodable {
+        
+        var codec_type: VideoCodecType
+        
+        public var codecType: VideoCodecType {
+            
+            get {
+                return codec_type
+            }
+            
+            set(newCodecType) {
+                codec_type = newCodecType
+            }
+            
+        }
+        
+        public func JSONString() -> String {
+            let dict = NSMutableDictionary()
+            dict["codec_type"] = codec_type.JSONString()
+            return try! String(data: NSJSONSerialization.dataWithJSONObject(dict, options: NSJSONWritingOptions(rawValue: 0)),
+                               encoding: NSUTF8StringEncoding)!
+        }
+        
+    }
+    
+    public enum AudioCodecType: JSONEncodable {
+        
         case Opus
+        
+        public func JSONString() -> String {
+            switch self {
+            case .Opus:
+                return "OPUS"
+            }
+        }
+        
+    }
+    
+    public struct Audio: JSONEncodable {
+        
+        public var codec_type: AudioCodecType
+        
+        public func JSONString() -> String {
+            let dict = NSMutableDictionary()
+            dict["codec_type"] = codec_type.JSONString()
+            return try! String(data: NSJSONSerialization.dataWithJSONObject(dict, options: NSJSONWritingOptions(rawValue: 0)),
+                               encoding: NSUTF8StringEncoding)!
+        }
+        
     }
     
     public struct Connect {
@@ -23,12 +127,28 @@ public enum Signaling {
         public var role: Role
         public var channelId: String
         public var accessToken: String?
-        /*
-        public var videoCodecType: VideoCodecType
-        public var audioCodecType: AudioCodecType
-        public var isVideoEnabled: Bool
-        public var isAudioEnabled: Bool
- */
+        public var video: Config<Video> = Config.Default
+        public var audio: Config<Audio> = Config.Default
+        
+        init(role: Role, channelId: String, accessToken: String?) {
+            self.role = role
+            self.channelId = channelId
+            self.accessToken = accessToken
+        }
+        
+        public func JSONString() -> String {
+            let dict = NSMutableDictionary()
+            dict["type"] = "connect"
+            dict["role"] = role.JSONString()
+            dict["channel_id"] = channelId
+            if let value = accessToken {
+                dict["access_token"] = value
+            }
+            if let value = video.JSONString() {
+                dict["video"] = value
+            }
+            return CreateJSONString(dict)
+        }
         
     }
     
@@ -46,8 +166,80 @@ public enum Signaling {
     
 }
 
+extension Signaling.Role: Decodable {
+    
+    public static func decode(j: JSON) -> Decoded<Signaling.Role> {
+        switch j {
+        case let .String(dest):
+            switch dest {
+            case "upstream":
+                return pure(.Upstream)
+            case "downstream":
+                return pure(.Downstream)
+            default:
+                return .typeMismatch("invalid destination", actual: dest)
+            }
+        default:
+            return .typeMismatch("String", actual: j)
+        }
+    }
     
 }
 
+extension Signaling.VideoCodecType: Decodable {
+    
+    public static func decode(j: JSON) -> Decoded<Signaling.VideoCodecType> {
+        switch j {
+        case let .String(name):
+            switch name {
+            case "VP8":
+                return pure(.VP8)
+            case "VP9":
+                return pure(.VP9)
+            case "H264":
+                return pure(.H264)
+            default:
+                return .typeMismatch("invalid video codec type", actual: name)
+            }
+        default:
+            return .typeMismatch("String", actual: j)
+        }
+    }
+    
+}
+
+extension Signaling.Video: Decodable {
+    
+    public static func decode(j: JSON) -> Decoded<Signaling.Video> {
+        return curry(Signaling.Video.init)
+            <^> j <| "codec_type"
+    }
+    
+}
+
+extension Signaling.AudioCodecType: Decodable {
+    
+    public static func decode(j: JSON) -> Decoded<Signaling.AudioCodecType> {
+        switch j {
+        case let .String(name):
+            switch name {
+            case "OPUS":
+                return pure(.Opus)
+            default:
+                return .typeMismatch("invalid audio codec type", actual: name)
+            }
+        default:
+            return .typeMismatch("String", actual: j)
+        }
+    }
+    
+}
+
+extension Signaling.Audio: Decodable {
+    
+    public static func decode(j: JSON) -> Decoded<Signaling.Audio> {
+        return curry(Signaling.Audio.init)
+            <^> j <| "codec_type"
+    }
     
 }
