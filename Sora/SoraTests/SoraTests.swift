f6febe7170ec2e6e8568aecfc603fc292dd8ce55
SUZUKI Tetsuya <szktty@shiguredo.jp>
2016-12-19 15:46:37 +0900

    一旦削除

--- Sora/SoraTests/SoraTests.swift
+++ Sora/SoraTests/SoraTests.swift
@@ -4,8 +4,6 @@ import WebRTC
 
 class SoraTests: XCTestCase {
     
-    var conn: Connection!
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,40 +14,9 @@ class SoraTests: XCTestCase {
         super.tearDown()
     }
     
-    func testConnecting() {
-        let exp = expectationWithDescription("connecting")
-        var result = false
-        conn = Sora.Connection(URL: NSURL(string: "ws://127.0.0.1:5000/signaling")!)
-        conn.connect { (error) in
-            if let error = error {
-                print("signaling connecting is failed: ", error)
-                return
-            }
-            print("signaling connection is open")
-            var channel = self.conn.createMediaChannel("sora")
-            channel.createMediaSubscriber {
-                (subscriber, error) in
-                
-                if let error = error {
-                    print("media channel could not connect: ", error)
-                    return
-                }
-                result = true
-                subscriber!.disconnect()
-                exp.fulfill()
-                print("media channel connected")
-            }
-        }
-        waitForExpectationsWithTimeout(5) {
-            (error) in
-            XCTAssert(error == nil)
-            XCTAssert(result)
-        }
-    }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
