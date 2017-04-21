79663ed04741234aa027a93984ccff5e08c88ed4
SUZUKI Tetsuya <szktty@shiguredo.jp>
2017-04-21 16:43:45 +0900

    EventLogViewController を追加する

---
+++ Sora/Sora/ConnectionController/EventLogViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class EventLogViewController: UITableViewController {
+
+    @IBOutlet weak var maxNumberOfLogsTextField: UITextField!
+    @IBOutlet weak var enableLoggingSwitch: UISwitch!
+    @IBOutlet weak var showURLSwitch: UISwitch!
+    @IBOutlet weak var showChannelIdSwitch: UISwitch!
+    @IBOutlet weak var showDateAndTimeSwitch: UISwitch!
+    @IBOutlet weak var showEventTypeSwitch: UISwitch!
+    @IBOutlet weak var showCommentSwitch: UISwitch!
+    @IBOutlet weak var filterWebSocketSwitch: UISwitch!
+    @IBOutlet weak var filterSignalingSwitch: UISwitch!
+    @IBOutlet weak var filterPeerConnectionSwitch: UISwitch!
+    @IBOutlet weak var filterConnectionMonitorSwitch: UISwitch!
+    @IBOutlet weak var filterMediaPublisherSwitch: UISwitch!
+    @IBOutlet weak var filterMediaSubscriberSwitch: UISwitch!
+    @IBOutlet weak var filterMediaStreamSwitch: UISwitch!
+    @IBOutlet weak var filterVideoRendererSwitch: UISwitch!
+    @IBOutlet weak var filterVideoViewSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
